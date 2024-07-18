c88cd3610a6f82071fcab511dfdeee67a2075b40
Alexey Slyusar <alttepkuh@yandex.ru>
2024-07-18 11:58:36 +0700

    minor styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,36 +6,43 @@ import IconButton from "@mui/material/IconButton";
 import styles from "../styles/main.module.scss";
 import { Router as RemixRouter } from "@remix-run/router/dist/router";
 import { HeaderMenu } from "./HeaderMenu";
+import { useMediaQuery } from "@mui/material";
 
 export default function Header({ router }: { router?: RemixRouter }) {
+  const mobileDevice = useMediaQuery("(max-width:767px)");
   return (
-    <div className={`${styles.headerWrapper} ${styles.standardShadow}`}>
-      <AppBar
-        position="static"
-        sx={{ maxWidth: "1200px", margin: "0 auto", boxShadow: "none" }}
-      >
-        <Toolbar sx={{ bgcolor: "rgba(79, 77, 77, 0.96)" }}>
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            color="inherit"
-          >
-            <img src={`./logo.png`} alt={`Main Logo`} className={styles.logo} />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, lineHeight: "1.3rem" }}
-          >
-            Клининговое Агентство
-            <br />
-            Господина Зузельштейна
-          </Typography>
-          <HeaderMenu />
-        </Toolbar>
-      </AppBar>
-    </div>
+    <AppBar position="static" sx={{ bgcolor: "rgba(79, 77, 77, 0.96)" }}>
+      <Toolbar>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <img src={`./logo.png`} alt={`Main Logo`} className={styles.logo} />
+        </IconButton>
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1, lineHeight: "1.3rem" }}
+        >
+          {!mobileDevice ? (
+            <span>
+              Клининговое Агентство
+              <br />
+              Господина Зузельштейна
+            </span>
+          ) : (
+            <span>
+              Агентство
+              <br />
+              Зузельштейна
+            </span>
+          )}
+        </Typography>
+        <HeaderMenu />
+      </Toolbar>
+    </AppBar>
   );
 }
